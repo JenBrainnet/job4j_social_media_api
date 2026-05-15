73a86c219f7d7cfb67b88cb197abd5f227264dc9
JenBrainnet <benebrak01@gmail.com>
2026-05-15 15:55:05 +0300

    Add JPQL repository queries

--- src/main/java/ru/job4j/socialmedia/repository/AccountRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/AccountRepository.java
@@ -1,7 +1,33 @@
 package ru.job4j.socialmedia.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.job4j.socialmedia.model.Account;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+
+    @Query("""
+            SELECT account FROM Account account
+            WHERE account.username = ?1 AND account.password = ?2
+            """
+    )
+    Optional<Account> findByUsernameAndPassword(String username, String password);
+
+    @Query("""
+            SELECT subscription.subscriber FROM Subscription subscription
+            WHERE subscription.subscribedAccount.id = ?1
+            """
+    )
+    List<Account> findSubscribersByAccountId(Integer accountId);
+
+    @Query("""
+            SELECT friendship.friend FROM Friendship friendship
+            WHERE friendship.owner.id = ?1
+            """
+    )
+    List<Account> findFriendsByAccountId(Integer accountId);
+
 }
--- src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
@@ -3,6 +3,9 @@ package ru.job4j.socialmedia.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.socialmedia.model.Post;
 
 import java.time.LocalDateTime;
@@ -15,5 +18,58 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByCreatedBetween(LocalDateTime start, LocalDateTime finish);
 
     Page<Post> findAllByOrderByCreatedDesc(Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            UPDATE Post post SET post.title = :title, post.text = :text
+            WHERE post.id = :postId
+            """
+    )
+    int updateTitleAndText(
+            @Param("postId") Integer postId,
+            @Param("title") String title,
+            @Param("text") String text
+    );
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            DELETE FROM PostImage image
+            WHERE image.id = :imageId AND image.post.id = :postId
+            """
+    )
+    int deleteImageByIdAndPostId(
+            @Param("imageId") Integer imageId,
+            @Param("postId") Integer postId
+    );
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+        DELETE FROM PostImage image
+        WHERE image.post.id = :postId
+        """
+    )
+    int deleteImagesByPostId(@Param("postId") Integer postId);
+
+    @Modifying(clearAutomatically = true)
+    @Query("""
+        DELETE FROM Post post
+        WHERE post.id = :postId
+        """
+    )
+    int deletePostById(@Param("postId") Integer postId);
+
+    @Query("""
+            SELECT post FROM Post post
+            WHERE post.account.id IN (
+                 SELECT subscription.subscribedAccount.id FROM Subscription subscription
+                 WHERE subscription.subscriber.id = :accountId
+            )
+            ORDER BY post.created DESC
+            """
+    )
+    Page<Post> findFeedPostsOrderByCreatedDesc(
+            @Param("accountId") Integer accountId,
+            Pageable pageable
+    );
 
 }
